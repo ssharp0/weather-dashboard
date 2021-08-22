9def2050f0f059ba0c0c21028a46cdeaa868dd91
SHARP <ssharp9999@gmail.com>
2021-08-21 22:05:22 -0700

    minor js setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,11 @@ console.log('today date: ' + currentDate);
 
 // global variables
 let searchCityWeatherBtn = document.getElementById("searchCityWeatherBtn")
+let clearSearchHistoryBtn = document.getElementById("clearSearchHistoryBtn")
+const apiKey = ''
+
+// storage
+
 
 
 // search weather when search button clicked to populate current weather
@@ -13,12 +18,15 @@ searchCityWeatherBtn.addEventListener('click', event => {
   event.preventDefault()
 
   let searchCityWeather = document.getElementById("searchCityWeather").value
+  console.log(`Search was: ${searchCityWeather}`);
 
-  axios.get(``)
-    .then(})
+  axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${searchCityWeather}&appid=${apiKey}`)
+    .then(res => {
+
+      // see properties
+      console.log(res);
+
+      document.getElementById('searchCityWeather').innerHTML =''
+    })
     .catch(err => console.log(err));
-
-
-
-
 })
